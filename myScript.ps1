5f8e41bc899183581d3465c7bd2b00f68077d873
rlthompson904 <41490785+rlthompson904@users.noreply.github.com>
2021-07-23 16:55:10 -0400

    Update myScript.ps1

--- myScript.ps1
+++ myScript.ps1
@@ -1,7 +1,12 @@
-function replacementIntoLCA($replacementToken){
+param
+(
+    [string]$arg1
+)
 
-$DesktopPath = [Environment]::GetFolderPath("Desktop")
+function SomeFunction
+{
+    $DesktopPath = [Environment]::GetFolderPath("Desktop")
 
-Set-Content -Path $DesktopPath\test.ps1 -value "$replacementToken"
-
+    Set-Content -Path $DesktopPath\test.ps1 -value "$arg1"
 }
+SomeFunction -arg1 $arg1 #Call the function from within SomeScript.ps1
